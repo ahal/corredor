550762703a8d07b541693dcacd0fc6c33306cd6e
Andrew Halberstadt <halbersa@gmail.com>
2014-04-03 09:01:59 -0400

    Refactor strategy/example to mimic potential marionette-js-runner host

--- corredor/strategy/pingpong.py
+++ corredor/strategy/pingpong.py
@@ -8,8 +8,12 @@ class PingPong(object):
 
     def __init__(self):
         self.output = OutputHandler('ipc', '/tmp/corredor_worker_output')
+        self.output.process_output()
         self.worker = ExclusivePair('ipc', '/tmp/corredor_exclusivepair')
         self.results = []
+        def append_result(result):
+            self.results.append(result)
+        self.worker.register_callback('test_end', append_result)
     
     def synchronize(self, action):
         data = {}
@@ -17,21 +21,14 @@ class PingPong(object):
             data = self.worker.recv_json()
         return data
 
-    def send_tests(self, tests):
-        def append_result(result):
-            self.results.append(result)
-        self.worker.register_callback('test_end', append_result)
-        self.synchronize('ready')
-        self.output.process_output()
+    def send_test(self, test):
+        self.worker.send_json({'action': 'test_start', 'test': test})
+        self.synchronize('test_end')
 
-        for test in tests:
-            time.sleep(1) # simulate some processing
-            self.worker.send_json({'action': 'test_start', 'test': test})
-            self.synchronize('test_end')
-
+    def finish(self):
         self.worker.send_json({'action': 'fin'})
         self.worker.cleanup()
 
         print 'Test results: %s' % json.dumps(self.results, indent=2)
-        print ''.join(self.output.stdout)
-        print ''.join(self.output.stderr)
+        print 'stdout:\n%s' % ''.join(self.output.stdout)
+        print 'stderr:\n%s' % ''.join(self.output.stderr)
--- test_program.py
+++ test_program.py
@@ -1,6 +1,23 @@
+"""
+Program emulates how the python side of the marionette-js-runner host
+might look. Sleeps instead of actually doing the work.
+"""
+
 from corredor.strategy import PingPong
+import time
 
 transport = PingPong()
 
+# figure out which tests to run
 tests = ['test/foo', 'test/bar', 'test/baz']
-transport.send_tests(tests)
+for test in tests:
+    # wait for profile to run
+    profile = transport.synchronize('profile')
+    # use profile to launch b2g_desktop/device
+    time.sleep(1)
+    # set up monitoring for timeouts/crashes
+    transport.send_test(test)
+    # do cleanup
+    time.sleep(1)
+
+transport.finish()
